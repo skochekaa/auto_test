04950a3e3e4f00bda75e4c0d5db784991c673100
a.skochek <aleksandrskochek@gmail.com>
2025-03-21 11:45:34 +0300

    Выолнено ДЗ 7 урока

---
+++ sk/hw7/task_2.py
@@ -0,0 +1,10 @@
+words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
+
+for key, value in words.items():
+    my_word = ""
+    for i in range(value):
+        my_word += key
+    print(my_word)
+
+
+
