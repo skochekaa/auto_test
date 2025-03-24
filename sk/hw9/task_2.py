9901fa704f6f2eee8a072032038dbf4c041de3c8
a.skochek <aleksandrskochek@gmail.com>
2025-03-24 23:06:56 +0300

    Исправлен print во второй задаче

--- sk/hw9/task_2.py
+++ sk/hw9/task_2.py
@@ -1,5 +1,7 @@
+import statistics
+
 temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22,
                 22, 23, 25, 29, 29, 31, 33, 31, 30, 32, 30, 28, 24, 23]
 # Создаем новый список с элементами значение которых больше 28
 hot_day = list(filter(lambda x: x > 28, temperatures))
-print(hot_day)
+print(f"Max t: {max(hot_day)}, min t: {min(hot_day)}, avg t: {round(statistics.mean(hot_day))}")
