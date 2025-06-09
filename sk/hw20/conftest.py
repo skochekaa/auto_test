2b33bb271a0f916a53a42089188c0bf78093e698
a.skochek <aleksandrskochek@gmail.com>
2025-06-09 15:46:45 +0300

    Начало выполенения ДЗ 20

---
+++ sk/hw20/conftest.py
@@ -0,0 +1,35 @@
+import requests
+import pytest
+import allure
+
+
+@pytest.fixture()
+def new_test_post():
+    """
+    Создает объект для работы с другими тестами и удаляет после завершения
+    """
+    json = {
+        "data": {
+            "color": "white",
+            "size": "big"
+        },
+        "name": "Alex"
+    }
+    response = requests.post("http://167.172.172.115:52353/object", json=json)
+    post_id = response.json()["id"]
+    yield post_id
+    requests.delete(f"http://167.172.172.115:52353/object/{post_id}")
+
+
+@pytest.fixture(scope="session")
+def status_all_test():
+    print("Start testing")
+    yield
+    print("Testing completed")
+
+
+@pytest.fixture()
+def status_test():
+    print("before test")
+    yield
+    print("after test")
